45d391a5fcaf91ee7b20742bfb901615fe9ae9c0
Pat <pat@dbolical.com>
2018-05-30 15:47:43 +1000

    Previous CSS changes.

--- source/javascripts/lib/_modio.js
+++ source/javascripts/lib/_modio.js
@@ -2,36 +2,26 @@ $(document).ready(function() {
 	/**
 	 Custom highlighting
 	 */
-	var target;
+	var highlightTarget;
 
 	$('tr').hover(function() {
 		if (! ($(this).children('th').length > 0)) {
 			$(this).addClass('shadow-drop-lr');
-			$(this).css({'cursor':'pointer'});
+			$(this).addClass('show-pointer');
 			var attribute = $(this).find('td:first-child').clone();
 			attribute.find('a').remove();
 			attribute = attribute.text();
 
-			if (attribute == '200') {
-				// Means we are in the "Response Schema" section under
-				// an endpoint and we don't need to do highlighing
-				return false;
-			}
+			var jsonExampleBlock = $(this).closest('table').prev().prev().find('code').children();
 
-			var codeblock = $(this).parent().parent().prev().prev().children().first().children();
-
-			$(codeblock).each(function(i, obj) {
+			$(jsonExampleBlock).each(function(i, obj) {
 				if ($(obj).first().hasClass('s2')) {
 					var col = $(obj).first();
 					attribute = attribute.replace(' ', '');
 
 					if ($(col).text() == '"'+attribute+'"') {
-						target = $(col);
-						$(target).css({
-							'color':'#fff',
-							'font-weight':'bold',
-							'border-bottom':'1px solid #44bfd4'
-						});
+						highlightTarget = $(col);
+            $(highlightTarget).addClass('json-example-highlight');
 						return false;
 					}
 				}
@@ -39,7 +29,7 @@ $(document).ready(function() {
 		}
 	}, function() {
 		$(this).removeClass('shadow-drop-lr');
-		$(target).css({'color':'#e6db74', 'font-weight':'normal', 'border-bottom':'none'});
+		$('span.s2').removeClass('json-example-highlight');
 	});
 	
 	$('table tbody tr').each(function() {
