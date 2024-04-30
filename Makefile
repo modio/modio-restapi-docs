73a149c2e0c4228629994abc2062d322f363579e
Jonathan Cheong <jon@mod.io>
2024-04-30 12:30:48 +1000

    chore: added additional scripts to copy the site into modio docs s3

---
+++ Makefile
@@ -0,0 +1,14 @@
+
+ENVIRONMENT?=dev
+SHELL=/bin/bash
+
+.PHONY: build clean deploy-into-modio-docs
+
+clean:
+	rm -rf restapi_docs_platform_output
+
+build:
+	bundle exec middleman build --clean
+
+deploy-into-modio-docs: clean build
+	./tools/create-slate-output.sh $(ENVIRONMENT)
---
+++ create-slate-output.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -o errexit #abort if any command fails
+
+environment=$1
+
+# Rename build folder docusaurus_build_output which acts as the root folder for the static website
+mkdir -p restapi_docs_platform_output
+
+# move folders into docusaurus_build_output folder and remove the -build suffix
+mv build restapi_docs_platform_output/build
+
+# Sync the build to the restapiref folder 
+aws s3 sync restapi_docs_platform_output/build s3://restapi-docs-"$environment"/restapiref
