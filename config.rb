aaeda113c25e45fe80f238ec17434a1efb6fd278
Jonathan Cheong <jon@mod.io>
2024-04-30 13:27:31 +1000

    chore: change assets path to restapiref

--- config.rb
+++ config.rb
@@ -15,10 +15,10 @@ set :markdown,
     renderer: UniqueHeadCounter
 
 # Assets
-set :css_dir, 'stylesheets'
-set :js_dir, 'javascripts'
-set :images_dir, 'images'
-set :fonts_dir, 'fonts'
+set :css_dir, 'restapiref/stylesheets'
+set :js_dir, 'restapiref/javascripts'
+set :images_dir, 'restapiref/images'
+set :fonts_dir, 'restapiref/fonts'
 
 # Activate the syntax highlighter
 activate :syntax
